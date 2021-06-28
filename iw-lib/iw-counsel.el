80f13729a7044faa899b62aa21119291f086a50e
iain <iain@soulflyer.co.uk>
2021-06-28 17:10:08 +0700

    added flx-ido

--- iw-lib/iw-counsel.el
+++ iw-lib/iw-counsel.el
@@ -27,5 +27,16 @@
   :ensure t
   :config (ivy-rich-mode 1))
 
+;; This makes delete in ido remove a path section rather than a character.
+(use-package flx-ido
+  :ensure t
+  :config
+  (ido-mode 1)
+  (ido-everywhere 1)
+  (flx-ido-mode 1)
+  ;; disable ido faces to see flx highlights.
+  (setq ido-enable-flex-matching t)
+  (setq ido-use-faces nil))
+
 (provide 'iw-counsel)
 ;;; iw-counsel.el ends here
