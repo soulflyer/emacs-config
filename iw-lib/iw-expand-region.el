c3b038879b87d842e4b656f5b18f0e4257118177
iain <iain@soulflyer.co.uk>
2021-09-30 01:32:38 +0700

    fixed expand region bindings for gui emacs

--- iw-lib/iw-expand-region.el
+++ iw-lib/iw-expand-region.el
@@ -3,14 +3,15 @@
 
 ;; Add binding to C-1 in iterm keymapping send hex codes 0x18 0x78 0x31
 ;; so xpand and contract region can be accessed using C-2 and C-1
-;; may need something else to get this working ok in GUI emacs. FIXME
 
 ;;; Code:
 (require 'use-package)
 (use-package expand-region
   :ensure t
-  :bind (("C-@" . er/expand-region)
-         ("C-x x 1" . er/contract-region)))
+  :bind (("C-@"     . er/expand-region)
+         ("C-2"     . er/expand-region)
+         ("C-x x 1" . er/contract-region)
+         ("C-1"     . er/contract-region)))
 
 (provide 'iw-expand-region)
 ;;; iw-expand-region.el ends here
