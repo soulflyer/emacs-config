1bc442fa8e074bf89d71c122220f5fc9d27ad724
iain <iain@soulflyer.co.uk>
2021-09-30 00:59:40 +0700

    fixed expand region bindings for terminal emacs

--- iw-lib/iw-expand-region.el
+++ iw-lib/iw-expand-region.el
@@ -1,13 +1,16 @@
 ;;; iw-expand-region --- setup expand region.
 ;;; Commentary:
-;;; xpand and contract region are accessed using C-2 and C-1
+
+;; Add binding to C-1 in iterm keymapping send hex codes 0x18 0x78 0x31
+;; so xpand and contract region can be accessed using C-2 and C-1
+;; may need something else to get this working ok in GUI emacs. FIXME
 
 ;;; Code:
 (require 'use-package)
 (use-package expand-region
   :ensure t
   :bind (("C-@" . er/expand-region)
-         ("C-]" . er/contract-region)))
+         ("C-x x 1" . er/contract-region)))
 
 (provide 'iw-expand-region)
 ;;; iw-expand-region.el ends here
