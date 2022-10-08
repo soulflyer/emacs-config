6981c19cf179a9390b6d2736d6afd09c62f63e74
iain <iain@soulflyer.co.uk>
2022-10-08 21:02:15 +0700

    Cleaned up calendar and diary display for GUI Emacs

--- iw-lib/iw-display-buffer.el
+++ iw-lib/iw-display-buffer.el
@@ -22,10 +22,11 @@
       '(("*undo-tree*"     display-buffer-in-direction
          (side . right)
          (window-width . 30))
-        ("*calendar*"      (display-buffer-reuse-window 
-                            display-buffer-below-selected))
-        ("diary"           (display-buffer-reuse-window 
-                            display-buffer-use-some-window))
+        ("*calendar*"      (display-buffer-reuse-window
+                            display-buffer-same-window))
+        ("diary"           (display-buffer-reuse-window
+                            display-buffer-in-direction)
+         (direction . up))
         ("Agenda Commands" display-buffer-below-selected)
         ("Org Select"      display-buffer-below-selected)
         ("CAPTURE-"        display-buffer-below-selected)))
