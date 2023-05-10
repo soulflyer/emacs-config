4c4cffa8e576d27b14568129b302c4bd20245127
iain <iain@soulflyer.co.uk>
2023-05-11 00:48:35 +0700

    Fixed ibuffer layout

--- iw-lib/iw-ibuffer.el
+++ iw-lib/iw-ibuffer.el
@@ -12,12 +12,33 @@
 
 (use-package ibuffer-git
   :ensure t
-  :config (setq ibuffer-formats '((mark modified read-only git-status-mini " "
+  :init (setq ibuffer-saved-filter-groups '(("modified"
+                                             ("modified" (visiting-file) (modified))
+                                             ("dired" (mode . dired-mode))
+                                             ("code" (derived-mode . prog-mode))
+                                             ("special" (name . "^\\*"))
+                                             ("org-mode" (mode . org-mode))))
+              ibuffer-saved-filters
+              '(("modified"
+                 (visiting-file)
+                 (modified))
+                ("org-mode"
+                 (mode . org-mode))
+                ("programming"
+                 (or
+                  (derived-mode . prog-mode)
+                  (mode . ess-mode)
+                  (mode . compilation-mode)))
+                ("text document"
+                 (and
+                  (derived-mode . text-mode)
+                  (not
+                   (starred-name))))))
+  :config (setq ibuffer-formats '((mark modified read-only " "
 				        (size 7 -1 :left :elide)
 				        (name))
 			          (mark filename))
-                ibuffer-saved-filter-groups '(("default" ("modified" (modified) (visiting-file))))
-                ibuffer-saved-filters '(("modified" (modified) (visiting-file))))
+                )
   :bind (("C-x b"   . ibuffer)
 	 ("C-x C-b" . ibuffer)))
 
